5ab418b2ae6c33dc7b44989edb0e1ef443ec27b4
Shandy <andy-n73@mail.ru>
2018-07-14 00:01:35 +0300

    добавляет удобную сборку gulp

---
+++ myproject/gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+
+gulp.task('sass', function() {
+  return gulp.src('app/scss/**/*.scss') // Получаем все файлы с окончанием .scss/sass в папке app/scss и дочерних директориях
+  //вот в этой строчке можно отключить общую компиляцию
+ .pipe(sass())
+ .pipe(gulp.dest('app/css'))
+ .pipe(browserSync.reload({
+ stream: true
+ }))
+});
+
+gulp.task('browserSync', function() {
+  browserSync({
+ server: {
+ baseDir: 'app'
+ },
+  })
+})
+
+
+gulp.task('watch', ['browserSync', 'sass'], function (){
+  gulp.watch('app/scss/**/*.scss', ['sass']);
+  // Обновляем браузер при любых изменениях в HTML или JS
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/js/**/*.js', browserSync.reload);
+});
